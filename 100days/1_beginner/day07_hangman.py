0425d5ca7db630bf0be4d9665e1cea3f5abd98db
dbal1107 <dbal1107@gmail.com>
2022-11-04 15:46:58 +0900

    **hangman다시하기

--- 100days/1_beginner/day07_hangman.py
+++ 100days/1_beginner/day07_hangman.py
@@ -1,64 +1,7 @@
 import random
+from day07_hangman_art import stages
+from day07_hangman_words import word_list
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-word_list = ["seoyoon", "dongsin", "seula", "heeseung", "yumi"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
@@ -68,29 +11,22 @@ display = []
 for _ in range(word_length):
     display += "_"
 
+for game in range(7):
+  guess = input("Guess a letter: ").lower()
+  for letter in chosen_word:
+    for i in range(len(chosen_word)):
+      if guess == chosen_word[i]:
+        display[i] = guess
+    cut = 0
+  if guess not in display :
+    cut += 1
+    print(stages[cut])
+    # if 
+  print(display)
+  print(stages[cut])
 
+  if ''.join(display) == chosen_word:
+    print('You win!!!\n')
+    break
 
-
-
-
-
-
-# for game in range(7):
-#   guess = input("Guess a letter: ").lower()
-#   for letter in chosen_word:
-#     for i in range(len(chosen_word)):
-#       if guess == chosen_word[i]:
-#         display[i] = guess
-#     cut = 0
-#   if guess not in display :
-#     cut += 1
-#     print(stages[cut])
-#     # if 
-#   print(display)
-#   # print(stages[hangman])
-
-#   if ''.join(display) == chosen_word:
-#     print('You win!!!\n')
-#     break
-
-# print('You lose..\n')
+print('You lose..\n')
